3c16cc68ae7dd742f8d450e736e02d7b82b687eb
Abhay <abhaypatil8055@gmail.com>
2024-03-13 15:11:48 +0530

    level-2
    level-2

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,26 @@
+var validateForm = (event) => {
+    event.preventDefault();
+    var userName = document.querySelector("#uname");
+    var userEmail = document.querySelector("#umail");
+
+    if (userName.value.trim() === "") {
+        alert("Please Enter your Name");
+        userName.focus();
+        return false;
+    }
+    if (!isValidEmail(userEmail.value.trim())) {
+        alert("Please Enter Valid Email address");
+        userEmail.focus();
+        return false;
+    }
+    alert("Form Submitted Successfully!!");
+}
+
+function isValidEmail(email) {
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return emailRegex.test(email);
+}
+
 var alertMsg = () => {
-    alert("Hey Mate Fill the Form First, Thank You !!");
+    alert("Hey Mate Fill the Below Form First, Thank You !!");
 }
